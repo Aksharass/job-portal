9c414e26817d755f8b43454c5f9d539f9e1b9901
Aksharass <aksharass128@gmail.com>
2025-08-12 16:16:26 +0530

    upload .env

--- job-portal-client/src/api/jobs.js
+++ job-portal-client/src/api/jobs.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const jobsAPI = axios.create({
-  baseURL: `${API_URL}/jobs`, // Updated to match backend route
+  baseURL: `${API_URL}`, // Updated to match backend route
   headers: { "Content-Type": "application/json" },
 });
 
